035b8b112fe37240e115688c96b3db4e0c50c00d
Shivam Raj Singh <48837530+shivam3009@users.noreply.github.com>
2021-08-17 16:08:19 +0530

    17-8-21

---
+++ Arrays and Strings/Medium/codeblock.h
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+ll inf = 1e9;
+
+vector<int> input()
+{
+    int n;
+    cout << "Enter the size of array : ";
+    cin >> n;
+    cout << "Enter elements in array : ";
+    vector<int> arr(n);
+    for(int i =0; i < n; i++){
+        cin >> arr[i];
+    }
+    return arr;
+}
+void print (vector<int> v){
+    int n = v.size();
+    for(int i =0; i < n; i++){       
+        cout<< v[i] << " ";
+    }
+    cout << "\n";
+}
+
+void print (vector<vector<int>> v){
+    int n = v.size();
+    int m = v[0].size();
+    
+    for(int i =0; i < n; i++){
+        cout << "\n";
+        for (int j = 0; j < m; j++){
+            cout<< v[i][j] << " ";
+        }
+    }
+}
